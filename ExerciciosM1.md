4ac05156f63462101d3a8c235c884adb899b8e7f
Ian CA <aragaoian@edu.univali.br>
2025-04-01 17:57:09 +0000

    ex3 com semáforo e inicio nos ex teóricos

---
+++ ExerciciosM1.md
@@ -0,0 +1,22 @@
+# Exercícios M1
+
+### 1. Quais são os três principais objetivos de um sistema operacional?
+- Gerenciar recursos;
+- Intermediar a comunicação entre o usuário e hardware (Camada de abstração);
+- Segurança.
+
+### 2. Enfatizamos a necessidade de um sistema operacional para fazer uso eficiente do hardware de computação. Quando é apropriado para o sistema operacional abandonar esse princípio e “desperdiçar” recursos? Por que esse sistema não é realmente um desperdício?
+- 
+
+### Como a distinção entre modo kernel e modo usuário funciona como uma forma rudimentar de sistema de proteção (segurança)
+- O modo kernel, que concede ao SO acesso total a CPU e hardware, é ativado quando há uma chamada de sistema. E essa troca de user mode para kernel mode é feita pela troca de um "bit de modo' no processador
+    - 0 para kernel mode
+    - 1 para user mode
+
+### Temporizadores podem ser usados para calcular a hora atual. Forneça uma breve descrição de como isso pode ser feito.
+- 
+
+### Como os sistemas em cluster diferem dos sistemas multiprocessados? O que é necessário para que duas máquinas pertencentes a um cluster cooperem para fornecer um serviço de alta disponibilidade?
+- 
+
+
--- Threads/thread_ex3_NOTION_SEMAPHORE.c
+++ Threads/thread_ex3_NOTION_SEMAPHORE.c
@@ -1,30 +1,30 @@
-// Compilar => gcc -o thread_ex3_NOTION_MUTEX thread_ex3_NOTION_MUTEX.c -lpthread
-// Ex 3 com mutex para resolver problema de sincronização
+// Compilar => gcc -o thread_ex3_NOTION_SEMAPHORE thread_ex3_NOTION_SEMAPHORE.c -lpthread
+// Ex 3 com semaphore para resolver problema de sincronização
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <unistd.h>
 
 #define MAX_THREAD 3
 #define MAX_COUNT 10
 int global = 0;
-pthread_mutex_t mutex;
-
+sem_t semaphore;
 
 void* sensor (void* param) {
     int local = 0;
     while(1){
         sleep(1);
-        pthread_mutex_lock(&mutex);
+        sem_wait(&semaphore); // mesmo que 'lock' do mutex
         if(global >= MAX_COUNT){
-            pthread_mutex_unlock(&mutex);
+            sem_post(&semaphore);
             break;
         }
         global++;
         local++;
         printf("Variável Local na thread %ld: %d | Global: %d\n", (long)param, local, global);
-        pthread_mutex_unlock(&mutex);
+        sem_post(&semaphore); // mesmo que 'unlock' do mutex
     }
     pthread_exit(0); 
 }
@@ -33,8 +33,8 @@ void* sensor (void* param) {
 int main() {
 
     pthread_t tid[MAX_COUNT];
-    pthread_mutex_init(&mutex, NULL);
-    
+    sem_init(&semaphore, 0, 1);
+
     for(long i = 0; i < MAX_THREAD; i++){ 
         pthread_create(&tid[i], NULL, sensor, (void*)i); 
     }
@@ -44,7 +44,7 @@ int main() {
     }
 
     printf("Variavel global: %d\n", global);
-    pthread_mutex_destroy(&mutex);
+    sem_destroy(&semaphore);
 
     return 0;
 }
